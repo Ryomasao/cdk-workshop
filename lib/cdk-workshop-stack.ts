2203409e682e7688781410d8f813c8d9de9323a1
Ryomasao <konoemario@gmail.com>
2021-10-18 13:34:25 +0900

    バリデーションまで

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,7 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda'
 import * as apigw from '@aws-cdk/aws-apigateway';
-import {HitCounter} from './hitcounter'
+import { TableViewer } from 'cdk-dynamo-table-viewer';
+import { HitCounter } from './hitcounter'
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -21,6 +22,11 @@ export class CdkWorkshopStack extends cdk.Stack {
 
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
+    })
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table
     })
   }
 }
--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -4,17 +4,29 @@ import * as dynamodb from '@aws-cdk/aws-dynamodb';
 
 export interface HitCounterProps {
 	downstream: lambda.IFunction
+	readCapacity?: number
 }
 
 export class HitCounter extends cdk.Construct {
+
 	public readonly handler: lambda.Function;
 
+	public readonly table: dynamodb.Table;
+
 	constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
-		super(scope, id)
+		if(props.readCapacity !== undefined && (props.readCapacity < 5 || props.readCapacity > 20)) {
+			throw new Error('readCapacity must be greater than 5 and less than 20')
+		}
+
+		super(scope, id);
 
 		const table = new dynamodb.Table(this, 'Hits', {
-			partitionKey: {name:'path', type: dynamodb.AttributeType.STRING}
-		})
+			partitionKey: {name:'path', type: dynamodb.AttributeType.STRING},
+			encryption: dynamodb.TableEncryption.AWS_MANAGED,
+			readCapacity: props.readCapacity ?? 5
+		});
+
+		this.table = table;
 
 		this.handler = new lambda.Function(this, 'HitCounterHandler', {
       runtime: lambda.Runtime.NODEJS_14_X,
@@ -24,7 +36,7 @@ export class HitCounter extends cdk.Construct {
 				DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
 				HITS_TABLE_NAME: table.tableName	,
 			}
-		})
+		});
 
 		// lambdaにdynamodbへのread/write可能なロールを設定
 		table.grantReadWriteData(this.handler);
